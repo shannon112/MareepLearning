7da5593a8bb5f59ae28ef1559610f531b9a260fd
shannon112 <b03505011@ntu.edu.tw>
2020-04-06 01:13:55 +0800

    update TA code

---
+++ hw3_classification_img/train.py
@@ -0,0 +1,205 @@
+import os
+import numpy as np
+import cv2
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+import time
+
+def readfile(path, label):    
+    image_dir = sorted(os.listdir(path))
+    x = np.zeros((len(image_dir), 128, 128, 3), dtype=np.uint8)
+    y = np.zeros((len(image_dir)), dtype=np.uint8)
+    for i, file in enumerate(image_dir):
+        img = cv2.imread(os.path.join(path, file))
+        x[i, :, :] = cv2.resize(img,(128, 128))
+        if label:
+          y[i] = int(file.split("_")[0])
+    # if label=true: train&valid
+    if label:
+      return x, y
+    # if label=false: test
+    else:
+      return x
+
+# reading training set, validation set, testing set
+workspace_dir = '/home/shannon/Downloads/food-11'
+print("Reading data")
+train_x, train_y = readfile(os.path.join(workspace_dir, "training"), True)
+print("Size of training data = {}".format(len(train_x)))
+val_x, val_y = readfile(os.path.join(workspace_dir, "validation"), True)
+print("Size of validation data = {}".format(len(val_x)))
+test_x = readfile(os.path.join(workspace_dir, "testing"), False)
+print("Size of Testing data = {}".format(len(test_x)))
+
+# training with data augmentation
+train_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(15), 
+    transforms.ToTensor(), # image to Tensor，value normalize to [0,1] (data normalization)
+])
+
+# testing without data augmentation
+test_transform = transforms.Compose([
+    transforms.ToPILImage(),                                    
+    transforms.ToTensor(),
+])
+
+class ImgDataset(Dataset):
+    def __init__(self, x, y=None, transform=None):
+        self.x = x
+        # label is required to be a LongTensor
+        self.y = y
+        if y is not None:
+            self.y = torch.LongTensor(y)
+        self.transform = transform
+    def __len__(self):
+        return len(self.x)
+    def __getitem__(self, index):
+        X = self.x[index]
+        if self.transform is not None:
+            X = self.transform(X)
+        if self.y is not None:
+            Y = self.y[index]
+            return X, Y
+        else:
+            return X
+
+batch_size = 128
+train_set = ImgDataset(train_x, train_y, train_transform)
+val_set = ImgDataset(val_x, val_y, test_transform)
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        # torch.nn.MaxPool2d(kernel_size, stride, padding)
+        # input dim [3, 128, 128]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1, 1),  # [64, 128, 128]
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [64, 64, 64]
+
+            nn.Conv2d(64, 128, 3, 1, 1), # [128, 64, 64]
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [128, 32, 32]
+
+            nn.Conv2d(128, 256, 3, 1, 1), # [256, 32, 32]
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [256, 16, 16]
+
+            nn.Conv2d(256, 512, 3, 1, 1), # [512, 16, 16]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),       # [512, 8, 8]
+            
+            nn.Conv2d(512, 512, 3, 1, 1), # [512, 8, 8]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),       # [512, 4, 4]
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(512*4*4, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 11)
+        )
+
+    def forward(self, x):
+        out = self.cnn(x)
+        out = out.view(out.size()[0], -1)
+        return self.fc(out)
+
+model = Classifier().cuda()
+loss = nn.CrossEntropyLoss() # due to classification task，we use CrossEntropyLoss
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001) # optimizer use Adam
+num_epoch = 200
+
+for epoch in range(num_epoch):
+    epoch_start_time = time.time()
+    train_acc = 0.0
+    train_loss = 0.0
+    val_acc = 0.0
+    val_loss = 0.0
+
+    model.train() # ensure model is at train model (enable Dropout, etc.)
+    for i, data in enumerate(train_loader):
+        optimizer.zero_grad()
+        train_pred = model(data[0].cuda()) # using model get prediction, actually call model' forward function
+        batch_loss = loss(train_pred, data[1].cuda()) # calculating loss ( prediction and label need to be on CPU or GPU togather）
+        batch_loss.backward() # back propagation get every weight's gradient
+        optimizer.step() # use gradient update weights
+
+        train_acc += np.sum(np.argmax(train_pred.cpu().data.numpy(), axis=1) == data[1].numpy())
+        train_loss += batch_loss.item()
+    
+    model.eval()
+    with torch.no_grad():
+        for i, data in enumerate(val_loader):
+            val_pred = model(data[0].cuda())
+            batch_loss = loss(val_pred, data[1].cuda())
+
+            val_acc += np.sum(np.argmax(val_pred.cpu().data.numpy(), axis=1) == data[1].numpy())
+            val_loss += batch_loss.item()
+
+        print('[%03d/%03d] %2.2f sec(s) Train Acc: %3.6f Loss: %3.6f | Val Acc: %3.6f loss: %3.6f' % \
+            (epoch + 1, num_epoch, time.time()-epoch_start_time, \
+             train_acc/train_set.__len__(), train_loss/train_set.__len__(), val_acc/val_set.__len__(), val_loss/val_set.__len__()))
+
+# train in training & validating set
+train_val_x = np.concatenate((train_x, val_x), axis=0)
+train_val_y = np.concatenate((train_y, val_y), axis=0)
+train_val_set = ImgDataset(train_val_x, train_val_y, train_transform)
+train_val_loader = DataLoader(train_val_set, batch_size=batch_size, shuffle=True)
+
+model_best = Classifier().cuda()
+loss = nn.CrossEntropyLoss() 
+optimizer = torch.optim.Adam(model_best.parameters(), lr=0.001)
+num_epoch = 200
+
+for epoch in range(num_epoch):
+    epoch_start_time = time.time()
+    train_acc = 0.0
+    train_loss = 0.0
+
+    model_best.train()
+    for i, data in enumerate(train_val_loader):
+        optimizer.zero_grad()
+        train_pred = model_best(data[0].cuda())
+        batch_loss = loss(train_pred, data[1].cuda())
+        batch_loss.backward()
+        optimizer.step()
+
+        train_acc += np.sum(np.argmax(train_pred.cpu().data.numpy(), axis=1) == data[1].numpy())
+        train_loss += batch_loss.item()
+
+    print('[%03d/%03d] %2.2f sec(s) Train Acc: %3.6f Loss: %3.6f' % \
+      (epoch + 1, num_epoch, time.time()-epoch_start_time, \
+      train_acc/train_val_set.__len__(), train_loss/train_val_set.__len__()))
+
+test_set = ImgDataset(test_x, transform=test_transform)
+test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+model_best.eval()
+prediction = []
+with torch.no_grad():
+    for i, data in enumerate(test_loader):
+        test_pred = model_best(data.cuda())
+        test_label = np.argmax(test_pred.cpu().data.numpy(), axis=1)
+        for y in test_label:
+            prediction.append(y)
+
+# Write the result to csv file
+with open("predict.csv", 'w') as f:
+    f.write('Id,Category\n')
+    for i, y in  enumerate(prediction):
+        f.write('{},{}\n'.format(i, y))
