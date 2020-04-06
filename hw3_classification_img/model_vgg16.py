6e4e719c76a901145cfaf985e41be3af7a64461e
shannon112 <b03505011@ntu.edu.tw>
2020-04-07 00:48:41 +0800

    vgg16 train in 224x224

--- hw3_classification_img/model_vgg16.py
+++ hw3_classification_img/model_vgg16.py
@@ -8,7 +8,7 @@ class Classifier(nn.Module):
         # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
         # torch.nn.MaxPool2d(kernel_size, stride, padding)
         # input dim [3, 128, 128]
-        img_size = 128
+        img_size = 224
         self.cnn = nn.Sequential(
             nn.Conv2d(3, 64, 3, 1, 1),  # [64, 128, 128]
             nn.BatchNorm2d(64),
@@ -64,7 +64,7 @@ class Classifier(nn.Module):
             nn.MaxPool2d(2, 2, 0),       # [512, 4, 4]
             #img_size /= 2
         )
-        
+
         img_size = int(img_size/32) # 2^pooling times
 
         self.fc = nn.Sequential(
