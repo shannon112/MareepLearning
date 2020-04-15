6c8625927dc2f6c54bd30aee5823826e74318526
shannon112 <b03505011@ntu.edu.tw>
2020-04-15 16:43:38 +0800

    rename

--- hw3_classification_img/hw3_test.sh
+++
@@ -1,5 +0,0 @@
-# TODO: create shell script
-echo $1 #: raw data directory
-echo $2 #: prediction file
-wget -O model/model_vgg16lite https://www.dropbox.com/s/yy7v913fu6vsbot/model_0.9848396501457726_ep150?raw=1
-python3 predict.py $1 $2 model/model_vgg16lite
--- hw3_classification_img/hw3_train.sh
+++
@@ -1,3 +0,0 @@
-# TODO: create shell script
-echo $1 #: raw data directory
-python3 train.py $1
