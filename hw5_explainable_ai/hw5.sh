f9d934fc37a83f75b840a8bcd80ad4d7dead8421
shannon112 <b03505011@ntu.edu.tw>
2020-04-22 02:26:28 +0800

    update bash

--- hw5_explainable_ai/hw5.sh
+++ hw5_explainable_ai/hw5.sh
@@ -1,5 +1,5 @@
 # TODO: create shell script
-echo $1 #: training label data
-echo $2 #: training unlabel data
-wget -O model/w2v_labeled.model https://www.dropbox.com/s/othu4mqfjq65qb0/w2v_labeled.model?raw=1
-python3 main.py $1 $2
+echo $1 #: Food dataset directory
+echo $2 #: Output images directory
+wget -O model/model_vgg16lite https://www.dropbox.com/s/yy7v913fu6vsbot/model_0.9848396501457726_ep150?raw=1
+python3 saliency_map.py $1 model/model_vgg16lite $2
