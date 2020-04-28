2843093fdf65bfb10440cfc204acc921a107e063
shannon112 <b03505011@ntu.edu.tw>
2020-04-28 17:08:54 +0800

    update defense visualization

--- hw6_adversarial_attack/defense.py
+++ hw6_adversarial_attack/defense.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
 
     window_sizes = [3,5,7]
     sigmas = [11,21,31]
+    fig, axs = plt.subplots(3, 4, figsize=(40, 6))
     for idx,window_size in enumerate(window_sizes):
         os.makedirs(os.path.join(output_dirname,"avg"+str(window_size)))
         os.makedirs(os.path.join(output_dirname,"gau"+str(window_size)))
@@ -42,3 +43,14 @@ if __name__ == '__main__':
 
             img_bil = cv2.bilateralFilter(img,window_size,sigmas[idx],sigmas[idx])
             cv2.imwrite(os.path.join(output_dirname,"bil"+str(window_size),file),img_bil)
+
+            if i==0:
+                axs[idx][0].imshow(img_avg)
+                axs[idx][0].set_title("avg"+str(window_size)+"x"+str(window_size))
+                axs[idx][1].imshow(img_gau)
+                axs[idx][1].set_title("gau"+str(window_size)+"x"+str(window_size)+" 0")
+                axs[idx][2].imshow(img_med)
+                axs[idx][2].set_title("med"+str(window_size)+"x"+str(window_size))
+                axs[idx][3].imshow(img_bil)
+                axs[idx][3].set_title("bil"+str(window_size)+"x"+str(window_size)+" "+str(sigmas[idx]))
+    plt.show()
