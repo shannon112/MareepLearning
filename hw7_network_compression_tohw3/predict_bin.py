190af0f4ad3324bdcac87077840606fa5f2cc8de
shannon112 <b03505011@ntu.edu.tw>
2020-05-05 17:36:17 +0800

    confrim teacher model acc

--- hw7_network_compression_tohw3/predict_bin.py
+++ hw7_network_compression_tohw3/predict_bin.py
@@ -4,11 +4,11 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
+import torchvision.models as models
 from torch.utils.data import DataLoader, Dataset
 
-from dataset import ImgDataset
-from dataset import testTransform
-from dataset import readfile
+from dataset import MyDataset
+from dataset import get_dataloader
 from model_StudentNet import StudentNet
 
 #filename
@@ -16,32 +16,30 @@ workspace_dir = sys.argv[1]
 output_filename = sys.argv[2]
 model_filename = sys.argv[3]
 
-print("Reading data")
-test_x = readfile(os.path.join(workspace_dir, "testing"), False)
-print("Size of Testing data = {}".format(len(test_x)))
+# get dataloader
+test_loader = get_dataloader(workspace_dir,'validation', batch_size=32)
 
 # testing configuration
-model_best = StudentNet(base=16).cuda() 
+#model_best = StudentNet(base=16).cuda() 
+model_best = models.resnet18(pretrained=False, num_classes=11).cuda()
 model_best.load_state_dict(torch.load(model_filename))
-
-# testing dataset
-batch_size = 48
-test_set = ImgDataset(test_x, transform=testTransform)
-test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
 # predict
 model_best.eval()
 prediction = []
 with torch.no_grad():
     for i, data in enumerate(test_loader):
-        test_pred = model_best(data.cuda())
+        inputs, labels = data
+        inputs = inputs.cuda()
+        labels = labels.cuda()
+        test_pred = model_best(inputs)
         test_label = np.argmax(test_pred.cpu().data.numpy(), axis=1)
         for y in test_label:
             prediction.append(y)
 
 # Write the result to csv file
 with open(output_filename, 'w') as f:
-    f.write('AuthorId,DuplicateAuthorIds\n')
+    f.write('Id,label\n')
     for i, y in  enumerate(prediction):
         f.write('{},{}\n'.format(i, y))
 print("saved into",output_filename)
