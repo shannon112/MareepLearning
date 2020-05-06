02c7463295cf6ce2b7854b73c0a9c3d429a4f69e
shannon112 <b03505011@ntu.edu.tw>
2020-05-06 12:14:03 +0800

    update train from scrateacher

--- hw7_network_compression_tohw3/train_from_scratch.py
+++ hw7_network_compression_tohw3/train_from_scratch.py
@@ -19,24 +19,12 @@ print("Reading data")
 train_dataloader = get_dataloader(workspace_dir,'training', batch_size=32)
 valid_dataloader = get_dataloader(workspace_dir,'validation', batch_size=32)
 
-# teacher CNN model with acc=88.4, size=47.2MB
-teacher_net = models.resnet18(pretrained=False, num_classes=11).cuda()
-#teacher_net.load_state_dict(torch.load('./model/teacher_resnet18.bin'))
-teacher_net.load_state_dict(torch.load('./model/teacher_resnet18_from_scratch.bin'))
-
-# student based on Depthwise & Pointwise Convolution Layer instead of regular CNN
 student_net = StudentNet(base=16).cuda() 
 print(student_net)
 #student_net.load_state_dict(torch.load('./model/student_model_deeper_205ep.bin'))
 #student_net.load_state_dict(torch.load('./model/student_custom_small.bin'))
 optimizer = optim.Adam(student_net.parameters(), lr=1*1e-3)
-
-def loss_fn_kd(outputs, labels, teacher_outputs, T=20, alpha=0.5):
-    # standard Cross Entropy
-    hard_loss = F.cross_entropy(outputs, labels) * (1. - alpha)
-    # KL Divergence of soft_label/T
-    soft_loss = nn.KLDivLoss(reduction='batchmean')(F.log_softmax(outputs/T, dim=1),  F.softmax(teacher_outputs/T, dim=1)) * (alpha * T * T)
-    return hard_loss + soft_loss
+#loss = nn.CrossEntropyLoss()
 
 def run_epoch(dataloader, update=True, alpha=0.5):
     total_num, total_hit, total_loss = 0, 0, 0
@@ -46,19 +34,17 @@ def run_epoch(dataloader, update=True, alpha=0.5):
         # inputs and soft/hard label data
         inputs = inputs.cuda()
         hard_labels = torch.LongTensor(hard_labels).cuda()
-        with torch.no_grad(): # teacher is only used to get score, do not need to backprop, saving memory
-            soft_labels = teacher_net(inputs)
         # training student net
         if update:
             logits = student_net(inputs)
-            loss = loss_fn_kd(logits, hard_labels, soft_labels, 20, alpha) # T=20 is from paper
+            loss = F.cross_entropy(logits, hard_labels)
             loss.backward()
             optimizer.step()    
         # validating student net
         else:
             with torch.no_grad():
                 logits = student_net(inputs)
-                loss = loss_fn_kd(logits, hard_labels, soft_labels, 20, alpha)
+                loss = F.cross_entropy(logits, hard_labels)
         # calculating loss and acc
         total_hit += torch.sum(torch.argmax(logits, dim=1) == hard_labels).item()
         total_loss += loss.item() * len(inputs)
@@ -66,7 +52,6 @@ def run_epoch(dataloader, update=True, alpha=0.5):
     return total_loss / total_num, total_hit / total_num
 
 # main traning loop
-teacher_net.eval() # always
 now_best_acc = 0
 for epoch in range(200):
     student_net.train()
