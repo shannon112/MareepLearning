1645a7d400af6f9e40135ca4957c5f6be10ba5f1
shannon112 <b03505011@ntu.edu.tw>
2020-05-07 15:17:29 +0800

    update bash

--- hw7_network_compression_tohw3/hw7_test.sh
+++ hw7_network_compression_tohw3/hw7_test.sh
@@ -1,8 +1,4 @@
 # TODO: create shell script
-echo $1 #: input dir
-echo $2 #: output img dir
-python3 main_simple.py $1 $2 0.1 1.0
-
-#validating ./submission/eps0.1m4
-#classification acc:  0.085 17 200
-#L-inf 5.55
+echo $1 #: <data directory> 
+echo $2 #: <prediction file>
+python3 predict_pkl.py $1 $2 model.bin
