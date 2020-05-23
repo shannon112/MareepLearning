92b037e22abfce50a87f6480e0801706cbde94cb
shannon112 <b03505011@ntu.edu.tw>
2020-05-23 10:47:11 +0800

    recovery old version of train

--- hw10_anomaly_detection_img/autoencoder_eval_cnn.py
+++ hw10_anomaly_detection_img/autoencoder_eval_cnn.py
@@ -15,9 +15,6 @@ from sklearn.cluster import MiniBatchKMeans
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
-from dataset_strong import Image_Dataset
-from dataset_strong import test_transform
-
 
 test_filename = sys.argv[1]
 model_filename = sys.argv[2]
@@ -25,11 +22,16 @@ batch_size = 128
 same_seeds(0)
 
 # load data
-testX = np.load(test_filename, allow_pickle=True)
-testX = torch.tensor(testX, dtype=torch.float) #-1~1
-img_dataset = Image_Dataset(testX, test_transform)
-img_dataloader = DataLoader(img_dataset, batch_size=1, shuffle=True)
+test = np.load(test_filename, allow_pickle=True)
+print("data", test.shape)
+# input shape
+y = test
 
+# make dataset
+data = torch.tensor(y, dtype=torch.float)
+test_dataset = TensorDataset(data)
+test_sampler = SequentialSampler(test_dataset)
+test_dataloader = DataLoader(test_dataset, sampler=test_sampler, batch_size=batch_size)
 
 # load model
 model = torch.load(model_filename, map_location='cuda')
@@ -37,8 +39,8 @@ model.eval()
 
 # extract latent vector
 latents = []
-for i, data in enumerate(img_dataloader): 
-    img = data.cuda()
+for i, data in enumerate(test_dataloader): 
+    img = data[0].transpose(3, 1).cuda()
     vec, output = model(img)
     if i == 0: latents = vec.view(img.size()[0], -1).cpu().detach().numpy()
     else: latents = np.concatenate((latents, vec.view(img.size()[0], -1).cpu().detach().numpy()), axis = 0)
@@ -71,19 +73,16 @@ for n in range(20):
 # Method 2: PCA to 2D
 pca = PCA(n_components=2).fit(latents)
 y_projected = pca.transform(latents)
-
 # judge by distribution
 y_KMeans = MiniBatchKMeans(n_clusters=1, random_state=0).fit(y_projected)
 y_cluster = y_KMeans.predict(y_projected)
 dist = np.sum(np.square(y_KMeans.cluster_centers_[0] - y_projected), axis=1)
 y_pred = dist
 print(y_KMeans.cluster_centers_[0])
-
 # judge by reconstruction error
 #y_reconstructed = pca.inverse_transform(y_projected)  
 #dist = np.sqrt(np.sum(np.square(y_reconstructed - latents).reshape(len(y), -1), axis=1))
 #y_pred = dist
-
 # plot clustering result
 X = y_projected[:, 0]
 Y = y_projected[:, 1]
@@ -91,14 +90,11 @@ plt.scatter(X, Y, s=1,c=y_cluster)
 plt.legend()
 plt.title("pca")
 plt.savefig("img/cnn_pca_result.png")
-
 # output result
 with open('submission/prediction_cnn_pca.csv', 'w') as f:
     f.write('id,anomaly\n')
     for i in range(len(y_pred)):
         f.write('{},{}\n'.format(i+1, y_pred[i]))
-
-
 # score = roc_auc_score(y_label, y_pred, average='micro')
 # score = f1_score(y_label, y_pred, average='micro')
 # print('auc score: {}'.format(score))
--- hw10_anomaly_detection_img/autoencoder_train_cnn.py
+++ hw10_anomaly_detection_img/autoencoder_train_cnn.py
@@ -1,3 +1,4 @@
+
 import sys
 import numpy as np
 import torch
@@ -6,53 +7,56 @@ from torch.utils.data import DataLoader
 from torch.optim import Adam
 from torch import nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
+from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler, TensorDataset)
 import torchvision.transforms as transforms
 
 from autoencoder_model import conv_autoencoder
 from autoencoder_model import conv_autoencoder_hw9
 from utils import same_seeds
-from dataset_strong import Image_Dataset
-from dataset_strong import train_transform
 
 same_seeds(0)
 train_filename = sys.argv[1]
 model_filename = sys.argv[2]
-trainX = np.load(train_filename, allow_pickle=True)
-print(trainX.shape)
+train = np.load(train_filename, allow_pickle=True)
+print(train.shape)
 
 # parameters
 num_epochs = 200
-batch_size = 64
-learning_rate = 1e-4
+batch_size = 128
+learning_rate = 1e-3
 
 # make dataset
-trainX = torch.tensor(trainX, dtype=torch.float) #-1~1
-img_dataset = Image_Dataset(trainX, train_transform)
-img_dataloader = DataLoader(img_dataset, batch_size=batch_size, shuffle=True)
+data = torch.tensor(train, dtype=torch.float)
+data = (data + 1) / 2
+train_dataset = TensorDataset(data)
+train_sampler = RandomSampler(train_dataset)
+train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=batch_size)
 
 # model
 model = conv_autoencoder().cuda()
-criterion = nn.MSELoss() #nn.MSELoss() #
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
+criterion = nn.BCELoss() #nn.MSELoss() #
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # main traning loop
 best_loss = np.inf
 model.train()
 for epoch in range(num_epochs):
-    for data in img_dataloader:
-        data = data.cuda()
-        _,output = model(data)
-        loss = criterion(output, data)
- 
+    for data in train_dataloader:
+        # transform input X
+        img = data[0].transpose(3, 1).cuda()
+        # ===================forward=====================
+        _,output = model(img)
+        # loss
+        loss = criterion(output, img)
+        # ===================backward====================
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
+        # ===================save====================
     if loss.item() < best_loss:
         best_loss = loss.item()
         torch.save(model, model_filename+str(epoch))
         print("save")
-    
+    # ===================log========================
     print('epoch [{}/{}], loss:{:.4f}'
             .format(epoch + 1, num_epochs, loss.item()))
