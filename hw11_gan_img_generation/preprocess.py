cd51bb8b61ff77649a0974a6985b4d13c3a40567
shannon112 <b03505011@ntu.edu.tw>
2020-05-29 18:10:09 +0800

    print dataset images

--- hw11_gan_img_generation/preprocess.py
+++ hw11_gan_img_generation/preprocess.py
@@ -36,3 +36,34 @@ def get_dataset(root):
     print("A sample in dataset",  dataset[0].shape)
     print("max and min in a image", max(dataset[0].view(-1)),min(dataset[0].view(-1)))
     return dataset
+
+
+import os
+import sys
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+from torch.autograd import Variable
+
+if __name__ == '__main__':
+    workspace_dir = sys.argv[1] #~/Downloads/faces/
+
+    # hyperparameters 
+    batch_size = 100
+
+    # dataloader (You might need to edit the dataset path if you use extra dataset.)
+    dataset = get_dataset(os.path.join(workspace_dir))
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+
+    # show images
+    for i, data in enumerate(dataloader):
+        imgs = data
+        print(data.shape)
+        filename = os.path.join("./img", 'dataset.jpg')
+        grid_img = torchvision.utils.make_grid(imgs, nrow=10)
+        plt.figure(figsize=(10,8))
+        plt.imshow(grid_img.permute(1, 2, 0))
+        plt.title("dataset randn sample")
+        plt.savefig(filename)
+        plt.show()
+        break
